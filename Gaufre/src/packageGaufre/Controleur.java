d4b0359599f48a6d228b16fea475a2bdbdf3c036
arcaman <christopher38c@hotmail.com>
2016-05-09 14:39:35 +0200

    Commit des fichiers de controleur et modele avec les fonctions associees

--- Gaufre/src/packageGaufre/Controleur.java
+++ Gaufre/src/packageGaufre/Controleur.java
@@ -1,5 +1,50 @@
 package packageGaufre;
 
+import java.awt.Point;
+
+import javax.print.attribute.standard.DocumentName;
+
 public class Controleur {
+
+	int joueurCourant;
+	static Model donneesJeu;
+
+	public boolean verifiePartieFinieOuNon() {
+
+		boolean partieFinie;
+
+		if (donneesJeu.tabGaufre[0][1] == false && donneesJeu.tabGaufre[1][0] == false) {
+			partieFinie = true; // le process du jeu consiste à supprimer du
+								// coin gauche en bas à droite. Du coup si ces
+								// deux cases sont vides,
+								// cela signifie que toute la gaufre est mangee
+		} else {
+			partieFinie = false;
+		}
+
+		return partieFinie;
+
+	}
+
+	public boolean verifieCoupValideOuNon(Point pointJouee) {
+		if (donneesJeu.tabGaufre[pointJouee.x][pointJouee.y] == true) {
+			// le point n a pas ete joue
+			return true;
+		} else { // le point a ete joue precedemment
+			return false;
+		}
+	}
+
+	public void jouer() {
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("it works !!!");
+
+		donneesJeu = new Model();
+		System.out.println("le contneu de base d une case vaut : " + donneesJeu.tabGaufre[1][1]);
+
+	}
 
 }
--- Gaufre/src/packageGaufre/Model.java
+++ Gaufre/src/packageGaufre/Model.java
@@ -1,5 +1,92 @@
 package packageGaufre;
 
+import java.awt.Point;
+import java.util.*;
+
 public class Model {
+
+	boolean[][] tabGaufre;
+	static int longueur = 5;
+	static int largeur = 3;
+	static int nbJoueurs = 2;
+
+	ArrayList<Boolean[][]> listeAnnuler;
+	ArrayList<Boolean[][]> listeRefaire;
+
+	public Model() {
+		this.tabGaufre = new boolean[longueur][largeur];
+		for (int i = 0; i < longueur; i++) {
+			for (int j = 0; j < largeur; j++) {
+				this.tabGaufre[i][j] = true;
+			}
+		}
+
+	}
+
+	public static int getLargeur() {
+		return largeur;
+	}
+
+	public ArrayList<Boolean[][]> getListeAnnuler() {
+		return listeAnnuler;
+	}
+
+	public ArrayList<Boolean[][]> getListeRefaire() {
+		return listeRefaire;
+	}
+
+	public static int getLongueur() {
+		return longueur;
+	}
+
+	public static int getNbJoueurs() {
+		return nbJoueurs;
+	}
+
+	public boolean[][] getTabGaufre() {
+		return tabGaufre;
+	}
+
+	public static void setLargeur(int largeur) {
+		Model.largeur = largeur;
+	}
+
+	public void setListeAnnuler(ArrayList<Boolean[][]> listeAnnuler) {
+		this.listeAnnuler = listeAnnuler;
+	}
+
+	public void setListeRefaire(ArrayList<Boolean[][]> listeRefaire) {
+		this.listeRefaire = listeRefaire;
+	}
+
+	public static void setLongueur(int longueur) {
+		Model.longueur = longueur;
+	}
+
+	public static void setNbJoueurs(int nbJoueurs) {
+		Model.nbJoueurs = nbJoueurs;
+	}
+
+	public void setTabGaufre(boolean[][] tabGaufre) {
+		this.tabGaufre = tabGaufre;
+	}
+
+	public void manger(Point pointJouee) {
+
+		for (int i = pointJouee.x; i < this.longueur; i++) {
+			for (int j = pointJouee.y; j < this.largeur; j++) {
+				this.tabGaufre[i][j] = false;
+			}
+		}
+
+	}
+
+	public void refaire() {
+
+	}
+
+	public void annuler() {
+
+	}
 
 }
